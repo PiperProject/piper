28709140e9dd7a789dbb65742af00d3877c43743
KDahlgren <kdahlgren15@gmail.com>
2017-09-14 14:58:17 -0700

    comments in driver. added checks for python deps in setup. ignoring swps.

--- src/drivers/piper.py
+++ src/drivers/piper.py
@@ -19,34 +19,75 @@ URL_SIMPLEJOIN = "https://github.com/PiperProject/simpleJoin.git"
 def main() :
   packageList = sys.argv[2:]
 
-  if sys.argv[1] == "install" :
+  if len( sys.argv ) > 2 :
 
-    for p in packageList :
-      if p == "aggsPack" :
-        URL = URL_AGGSPACK
-        os.system( "cd " + PACKAGE_PATH + "; git submodule add -f " + URL + ";" )
-        os.system( "python " + CURR_PATH + "/setup.py" )
-      elif p == "simpleJoin" :
-        URL = URL_SIMPLEJOIN
-        os.system( "cd " + PACKAGE_PATH + "; git submodule add -f " + URL + ";" )
-        os.system( "python " + CURR_PATH + "/setup.py" )
-      else :
-        print "Package not recognized : " + p
+    # --------------------------------- #
+    #    INSTALL PACKAGES FROM INDEX    #
+    # --------------------------------- #
+    if sys.argv[1] == "install" :
 
-  elif sys.argv[1] == "uninstall" :
-    for p in packageList :
-      if p == "aggsPack" :
-        os.system( "cd " + PACKAGE_PATH + "; git rm --cached -f ./" + p + " ; rm -rf " + p + ";" )
-      elif p == "simpleJoin" :
-        os.system( "cd " + PACKAGE_PATH + "; git rm --cached -f ./" + p + " ; rm -rf " + p + ";" )
-      else :
-        print "Package not recognized : " + p
+      for p in packageList :
 
-  elif sys.argv[1] == "update" :
-    os.system( "python " + CURR_PATH + "/setup.py" )
+        # AGGSPACK
+        if p == "aggsPack" :
+          URL = URL_AGGSPACK
+          os.system( "cd " + PACKAGE_PATH + "; git submodule add -f " + URL + ";" )
+          os.system( "python " + CURR_PATH + "/setup.py" )
+
+        # SIMPLEJOIN
+        elif p == "simpleJoin" :
+          URL = URL_SIMPLEJOIN
+          os.system( "cd " + PACKAGE_PATH + "; git submodule add -f " + URL + ";" )
+          os.system( "python " + CURR_PATH + "/setup.py" )
+
+        # ERROR : UNKNOWN PACKAGE
+        else :
+          print "Package not recognized : " + p
+
+    # --------------------------------- #
+    #   UNINSTALL PACKAGES FROM INDEX   #
+    # --------------------------------- #
+    elif sys.argv[1] == "uninstall" :
+      for p in packageList :
+
+        # AGGSPACK
+        if p == "aggsPack" :
+          os.system( "cd " + PACKAGE_PATH + "; git rm --cached -f ./" + p + " ; rm -rf " + p + ";" )
+
+        # SIMPLEJOIN
+        elif p == "simpleJoin" :
+          os.system( "cd " + PACKAGE_PATH + "; git rm --cached -f ./" + p + " ; rm -rf " + p + ";" )
+
+        # ERROR : UNKNOWN PACKAGE
+        else :
+          print "Package not recognized : " + p
+
+    # --------------------------------- #
+    #   UPDATE ALL INSTALED PACKAGES    #
+    # --------------------------------- #
+    elif sys.argv[1] == "update" :
+      os.system( "python " + CURR_PATH + "/setup.py" )
+
+    # --------------------------------- #
+    #   ERROR : PACKAGE NOT RECOGNIZED  #
+    # --------------------------------- #
+    else :
+      sys.exit( "ERROR : unrecognized argument '" + sys.argv[1]  + "'. aboriting..." )
+
+
+  # --------------------------------- #
+  #   ERROR : NO ARGUMENTS PROVIDED   #
+  # --------------------------------- #
+  else :
+    sys.exit( "ERROR : must provide at least one of :\n{ 'install', 'uninstall' },\nwith appropriate corresponding additional arguments when calling the piper package index." )
 
 
 ##############################
 #  MAIN THREAD OF EXECUTION  #
 ##############################
 main()
+
+
+#########
+#  EOF  #
+#########
--- src/drivers/setup.py
+++ src/drivers/setup.py
@@ -6,7 +6,56 @@ import os, sys
 
 # -------------------------------------- #
 
-# keep all submodules up-to-date
-os.system( "git pull --recurse-submodules" )
-os.system( "git submodule update --remote --recursive" )
 
+
+###################
+#  CHECK PY DEPS  #
+###################
+# check python package dependencies
+def checkPyDeps() :
+
+  print "*******************************"
+  print "  CHECKING PYTHON DEPENDECIES  "
+  print "*******************************"
+
+  # pymongo
+  import pymongo
+  if pymongo.__name__ :
+    print "pymongo...verified"
+
+  # pickledb
+  import pickledb
+  if pickledb.__name__ :
+    print "pickledb...verified"
+
+  print "All python dependencies installed! Yay! =D"
+  print "*******************************"
+  print "*******************************"
+
+  return None
+
+
+##############################
+#  UPDATE EXISTING PACKAGES  #
+##############################
+def updateExistingPackages() :
+
+  print "*******************************"
+  print "  UPDATING EXISTING PACKAGES"
+  print "*******************************"
+
+  # keep all submodules up-to-date
+  os.system( "git pull --recurse-submodules" )
+  os.system( "git submodule update --remote --recursive" )
+
+
+##############################
+#  MAIN THREAD OF EXECUTION  #
+##############################
+checkPyDeps()
+updateExistingPackages()
+
+
+#########
+#  EOF  #
+#########
